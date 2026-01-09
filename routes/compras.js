6d6a47c46816662137f5284640bd7a9983cccfa8
pichon1984 <pichontecnico@gmail.com>
2026-01-09 12:04:39 -0300

    se conecta vercel3

--- routes/compras.js
+++ routes/compras.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Compra = require("../models/compra");
 const Producto = require("../models/producto");
-const Orden = require("../models/orden"); // 👈 si tenés el modelo de Orden
+const Orden = require("../models/orden"); 
 const { validarJWT } = require("../middlewares/validar-jwt");
 const { calcularCostoEnvio } = require("../helpers/envio");
 
